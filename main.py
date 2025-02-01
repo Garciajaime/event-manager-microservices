5669e15d7c38b4de2ab0552f5bec7990e8dc5bf6
Garciajaime <garciaab@oregonstate.edu>
2025-01-31 18:22:58 -0800

    Fixed a bug with the login. I now append username and password to the existing json object instead of adding another dictionary.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-
+import textwrap
 
 while True:
     loginVerified = False
@@ -20,17 +20,21 @@ while True:
         create_login_input = input('Want to create a login? (y/n): ')
         # create login
         if create_login_input == 'y':
+            opening_statement = '''
+            *Note: We will not share your login info with third party sites.
+            Login info will only be used to verify you as a user. 
+            '''
+            print(textwrap.dedent(opening_statement))
             username = input('Enter a username: ')
             password = input('Enter a password: ')
             passConfirm = None
             while password != passConfirm:
                 passConfirm = input('Please confirm your password: ')
-            loginCredentials = { username: password,
-
-            }
-            with open("login.txt","a", encoding="utf-8") as loginFile:
-                json.dump(loginCredentials, loginFile, indent=4)
-
+            with open("login.txt","r", encoding="utf-8") as loginFile:
+                loginData = json.load(loginFile)
+                loginData[username] = password            # add new entry to json object
+            with open("login.txt","w", encoding="utf-8") as loginFile:
+                json.dump(loginData,loginFile, indent=4)  # update json object in login file
     if loginVerified is True:
         print()
         print()
