6c8774b2e4b3a119aee7aaba8b3274ba63fa2ba9
Garciajaime <garciaab@oregonstate.edu>
2025-01-31 17:54:05 -0800

    Created a way to create a login and have your login verified.

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import json
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+while True:
+    loginVerified = False
+    login_input = input('Do you have a login? (y/n): ')
+    if login_input == 'y':
+        username = input('What is your username?: ')
+        password = input('What is your password?: ')
+        with open('login.txt','r',encoding='utf-8') as loginFile:
+            loginData = json.load(loginFile)
+            if username in loginData:
+                if loginData[username] == password:
+                    loginVerified = True
+                else:
+                    print('ERROR: Your password was incorrect')
+            else:
+                print('ERROR: Your username or password was incorrect')
+    elif login_input == 'n':
+        create_login_input = input('Want to create a login? (y/n): ')
+        # create login
+        if create_login_input == 'y':
+            username = input('Enter a username: ')
+            password = input('Enter a password: ')
+            passConfirm = None
+            while password != passConfirm:
+                passConfirm = input('Please confirm your password: ')
+            loginCredentials = { username: password,
+
+            }
+            with open("login.txt","a", encoding="utf-8") as loginFile:
+                json.dump(loginCredentials, loginFile, indent=4)
+
+    if loginVerified is True:
+        print()
+        print()
+        print('Congratulations you are logged in!')
+        print()
+        print()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
